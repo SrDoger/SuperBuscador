e484a5aad255b4a06e821bdb41c087e9ba4b2167
SrDoger <74317397+SrDoger@users.noreply.github.com>
2022-05-11 16:05:05 -0300

    Delete Buscador/buscadores/eBay-API-PHP-main directory

--- Buscador/buscadores/eBay-API-PHP-main/public_html/js/application.js
+++
@@ -1,9 +0,0 @@
-// Set the navigation to active for the current page
-$(document).ready(function(){
-    $('a').each(function(){
-        if ($(this).prop('href') == window.location.href) {
-            $(this).addClass('active'); 
-            $(this).parents('li').addClass('active');
-        }
-    });
-});
