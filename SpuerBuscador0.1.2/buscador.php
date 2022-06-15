9a06114f0c5be276496d64a6de4b1618c558b570
DR4Che <dariel0112cc@gmail.com>
2022-06-15 16:28:37 -0300

    SuperBuscador 0.1.4

--- SpuerBuscador0.1.2/buscador.php
+++ SpuerBuscador0.1.2/buscador.php
@@ -47,7 +47,7 @@
                 <div class="col-md-9">
                     <ul class="nav navbar-nav nabar">
                       <li class="active"><a href="index.html">Inicio</a></li>
-                      <li><a href="buscadores/buscador.html">Tienda</a></li>
+                      <li><a href="buscador.php">Tienda</a></li>
                       <li><a href="#">Caracteristicas</a></li>
                       <li><a href="#">Paginas</a></li>
                     </ul>
@@ -64,63 +64,62 @@
     </section>
 </header>
 <div class="cabeza">
-<?php
+    <?php
+        if (isset($_POST['mercadolibre'])){
+            echo '<div id="mercadolibre">';
+            $mlid = str_replace(" ", "%20", $_POST['element']);
+            $data = json_decode(file_get_contents('https://api.mercadolibre.com/sites/MLA/search?q='.$mlid,'&Minimum-price=3000'), true);
+        
+            $valor = 0;
+            foreach ($data["results"] as $i)
+            {
+                $valor += 1;
+                foreach ($i as $a["id"])
+                {
+                
+                    $mlid = $a["id"];
 
-if (isset($_POST['mercadolibre'])){
-    echo '<div id="mercadolibre">';
-    $mlid = str_replace(" ", "%20", $_POST['element']);
-    $data = json_decode(file_get_contents('https://api.mercadolibre.com/sites/MLA/search?q='.$mlid,'&Minimum-price=3000'), true);
+                    $data = json_decode(file_get_contents('https://api.mercadolibre.com/items/'.$mlid), true);
+                    echo '<div class="containerMerc">';
+                    print $a["id"];     
+                    echo '<br>';
+                    echo $data['title'];  
+                    echo '<br>';
+                    echo '<img class="imgPrincipal" style="height: 200px; width: 200px;" src="', $data['thumbnail'], '">' ;
+                    echo '<br>';
+                    echo '<p>Precio: '.$data['price'],'$</p>';
 
-    $valor = 0;
-    foreach ($data["results"] as $i)
-    {
-        $valor += 1;
-        foreach ($i as $a["id"])
-        {
+                    echo '<br>';
+                    echo '<div class="imgSecundarias">';
+                    foreach ($data['pictures'] as $j){   
+                        //echo '<p>'.$j['url'], '</p>';
+                    // echo '<img class="imgPrincipal" style="height: 200px; width: 200px;" src="', $data['url'], '">' ;
+                    
+                        //to do: poner las imagenes extra  
+                    }
+                    echo '</div>';
+                    /*echo '<div class="atributos">';
+                    foreach($data['attributes'] as $aux){
+                        $string = $aux['name'].': '.$aux['value_name'];
+                        print_r($string);
 
-            $mlid = $a["id"];
-            
-            $data = json_decode(file_get_contents('https://api.mercadolibre.com/items/'.$mlid), true);
-            echo '<div class="containerMerc">';
-            print $a["id"];     
-            echo '<br>';
-            echo $data['title'];  
-            echo '<br>';
-            echo '<img class="imgPrincipal" style="height: 200px; width: 200px;" src="', $data['thumbnail'], '">' ;
-            echo '<br>';
-            echo '<p>Precio: '.$data['price'],'$</p>';
-            
-            echo '<br>';
-            echo '<div class="imgSecundarias">';
-            foreach ($data['pictures'] as $j){   
-                //echo '<p>'.$j['url'], '</p>';
-            // echo '<img class="imgPrincipal" style="height: 200px; width: 200px;" src="', $data['url'], '">' ;
-            
-                //to do: poner las imagenes extra  
+                        echo '<br>';
+
+                    }
+                    echo '</div>';;*/
+                    echo '<hr>';
+                    echo '</div>';;
+                    break;
+
+                }
+                if($valor >= 10){
+                    break;
+                }
+
             }
             echo '</div>';
-            /*echo '<div class="atributos">';
-            foreach($data['attributes'] as $aux){
-                $string = $aux['name'].': '.$aux['value_name'];
-                print_r($string);
-                
-                echo '<br>';
-                
-            }
-            echo '</div>';;*/
-            echo '<hr>';
-            echo '</div>';;
-            break;
-            
         }
-        if($valor >= 10){
-            break;
-        }
-        
-    }
-    echo '</div>';
-}
-?>
+    ?>
 </div>
 <footer>
     <div class="footer_top">
