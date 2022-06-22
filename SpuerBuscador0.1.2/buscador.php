62ec0f3a1fcf38160ad6b41413f2c69c0a21e032
DR4Che <dariel0112cc@gmail.com>
2022-06-22 16:46:28 -0300

    SuperBuscador 0.2.1

--- SpuerBuscador0.1.2/buscador.php
+++ SpuerBuscador0.1.2/buscador.php
@@ -63,62 +63,10 @@
         </div>
     </section>
 </header>
-<div class="cabeza">
+<div class="cabeza" style="height: auto !important;">
     <?php
-        if (isset($_POST['mercadolibre'])){
-            echo '<div id="mercadolibre">';
-            $mlid = str_replace(" ", "%20", $_POST['element']);
-            $data = json_decode(file_get_contents('https://api.mercadolibre.com/sites/MLA/search?q='.$mlid,'&Minimum-price=3000'), true);
-        
-            $valor = 0;
-            foreach ($data["results"] as $i)
-            {
-                $valor += 1;
-                foreach ($i as $a["id"])
-                {
-                
-                    $mlid = $a["id"];
-
-                    $data = json_decode(file_get_contents('https://api.mercadolibre.com/items/'.$mlid), true);
-                    echo '<div class="containerMerc">';
-                    print $a["id"];     
-                    echo '<br>';
-                    echo $data['title'];  
-                    echo '<br>';
-                    echo '<img class="imgPrincipal" style="height: 200px; width: 200px;" src="', $data['thumbnail'], '">' ;
-                    echo '<br>';
-                    echo '<p>Precio: '.$data['price'],'$</p>';
-
-                    echo '<br>';
-                    echo '<div class="imgSecundarias">';
-                    foreach ($data['pictures'] as $j){   
-                        //echo '<p>'.$j['url'], '</p>';
-                    // echo '<img class="imgPrincipal" style="height: 200px; width: 200px;" src="', $data['url'], '">' ;
-                    
-                        //to do: poner las imagenes extra  
-                    }
-                    echo '</div>';
-                    /*echo '<div class="atributos">';
-                    foreach($data['attributes'] as $aux){
-                        $string = $aux['name'].': '.$aux['value_name'];
-                        print_r($string);
-
-                        echo '<br>';
-
-                    }
-                    echo '</div>';;*/
-                    echo '<hr>';
-                    echo '</div>';;
-                    break;
-
-                }
-                if($valor >= 10){
-                    break;
-                }
-
-            }
-            echo '</div>';
-        }
+        include 'Buscador/Mercadolibre.php';
+        include 'Buscador/Ebay.php';
     ?>
 </div>
 <footer>
