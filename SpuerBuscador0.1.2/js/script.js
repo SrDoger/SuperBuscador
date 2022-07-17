151d55c99faba81c41605eba316023f6ecb734db
DR4Che <dariel0112cc@gmail.com>
2022-07-17 16:06:38 -0300

    0.1.15

--- SpuerBuscador0.1.2/js/script.js
+++ SpuerBuscador0.1.2/js/script.js
@@ -1,34 +1,18 @@
-let modal;
-document.addEventListener("click", (e) => {
-  if (e.target.className === "modal-open") {
-    modal = document.getElementById(e.target.dataset.id);
-    openModal(modal);
-  } else if (e.target.className === "modal-close") {
-    closeModal(modal);
-  } else {
-    return;
-  }
+const openl = document.querySelector('.register🖊');
+const openr =document.querySelector('.Log-in')
+const login =document.querySelector('#login');
+const register = document.querySelector('#register');
+
+
+openl.addEventListener('click',(e)=>{
+  e.preventDefault();
+  login.classList.add('show');
+  register.classList.remove('show');
 });
 
-const openModal = (modal) => {
-    document.body.style.overflow = "hidden";
-    modal.setAttribute("open", "true");
-    document.addEventListener("keydown", escClose);
-    let overlay = document.createElement("div");
-    overlay.id = "modal-overlay";
-    document.body.appendChild(overlay);
-};
-
-const closeModal = (modal) => {
-  document.body.style.overflow = "auto";
-  modal.removeAttribute("open");
-  document.removeEventListener("keydown", escClose);
-  document.body.removeChild(document.getElementById("modal-overlay"));
-};
-
-const escClose = (e) => {
-  if (e.keyCode == 27) {
-    closeModal();
-  }
-};
+openr.addEventListener('click',(e)=>{
+  e.preventDefault();
+  login.classList.remove('show');
+  register.classList.add('show');
+});
 
