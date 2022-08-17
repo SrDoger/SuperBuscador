bc132a46bdd20aa444728b12fc65290a256d141f
SrDoger <74317397+SrDoger@users.noreply.github.com>
2022-08-17 17:46:49 -0300

    0.1.5
    Actualizacion del ticket pdf

--- ASIO/Buscador/algo.php
+++ ASIO/Buscador/algo.php
@@ -3,41 +3,86 @@ require('C:\xampp\htdocs\SuperBuscador\fpdf184\fpdf.php');
 class PDF extends FPDF
 {
 
-function Header()
-{
+    function Header()
+    {
 
-    // Arial bold 15
-    $this->SetFont('Arial','B',35);
-    // Move to the right
-    $this->Cell(70);
-    // Title
-    $this->Cell(30,10,'A.S.I.O.');
-    
-    // Line break
-}
+        // Arial bold 15
+        $this->SetFont('Arial','B',35);
+        // Move to the right
+        $this->Cell(70);
+        // Title
+        $this->Cell(30,10,'A.S.I.O.');
+        $this->Ln();
+        $this->Ln();
+        // Line break
+    }
 
-function Body(){
-    $pdf->SetFont('Arial','B',16);
-$pdf->Cell(40,10,'¡Hola, Mundo!');
+    function Footer()
+    {
+        // Position at 1.5 cm from bottom
+        $this->SetY(-15);
+        // Arial italic 8
+        $this->SetFont('Arial','I',8);
+        // Page number
+        $this->Cell(0,10,'Pagina '.$this->PageNo().'/{nb}',0,0,'C');
+    }
+
+    function LoadData($file)
+    {
+        // Leer las líneas del fichero
+        $lines = file($file);
+        $data = array();
+        foreach($lines as $line)
+            $data[] = explode(';',trim($line));
+        return $data;
+    }
+
+    function FancyTable($header, $data)
+    {
+        // Colores, ancho de línea y fuente en negrita
+        $this->SetFillColor(255,0,0);
+        $this->SetTextColor(255);
+        $this->SetDrawColor(128,0,0);
+        $this->SetLineWidth(.3);
+        $this->SetFont('','B');
+        // Cabecera
+        $w = array(40, 45, 45, 40);
+        for($i=0;$i<count($header);$i++)
+            $this->Cell($w[$i],7,$header[$i],1,0,'C',true);
+        $this->Ln();
+        // Restauración de colores y fuentes
+        $this->SetFillColor(224,235,255);
+        $this->SetTextColor(0);
+        $this->SetFont('');
+        // Datos
+        $fill = false;
+        foreach($data as $row)
+        {
+            $this->Cell($w[0],6,$row[0],'LR',0,'L',$fill);
+            $this->Cell($w[1],6,$row[1],'LR',0,'L',$fill);
+            $this->Cell($w[2],6,number_format($row[2]),'LR',0,'R',$fill);
+            $this->Cell($w[3],6,number_format($row[3]),'LR',0,'R',$fill);
+            $this->Ln();
+            $fill = !$fill;
+        }
+        // Línea de cierre
+        $this->Cell(array_sum($w),0,'','T');
+    }
+
 }
-function Footer()
-{
-    // Position at 1.5 cm from bottom
-    $this->SetY(-15);
-    // Arial italic 8
-    $this->SetFont('Arial','I',8);
-    // Page number
-    $this->Cell(0,10,'Pagina '.$this->PageNo().'/{nb}',0,0,'C');
-}
-}
+$pdf = new PDF();
+$pdf->Ln();
+$pdf->SetFont('Arial','',14);
+$header = array('Producto', 'Empresa', 'ID', 'Precio');  
+$data = $pdf->LoadData('paises.txt');
 
 
 // Instanciation of inherited class
-$pdf = new PDF();
+
 $pdf->AliasNbPages();
 $pdf->AddPage();
-$pdf->Body();
-$pdf->SetFont('Times','',12);
+$pdf->FancyTable($header,$data);
+
 $pdf->Output();
 ?>
 
