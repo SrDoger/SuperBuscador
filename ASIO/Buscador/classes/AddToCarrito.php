7c26bf8ed1c960ec82772ea8ec6ab29f0f8fd12f
DR4Che <53042878+DR4Che@users.noreply.github.com>
2022-11-02 14:27:50 -0300

    Delete ASIO directory

--- ASIO/Buscador/classes/AddToCarrito.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once("Conexion.php");
-require_once("Session.php");
-$carrito = new Carrito();
-$session = new session();
-$carrito->addCarrito($_POST["productoID"], $_SESSION["id"]);
-$carrito->addCarrito($_POST["productoID"], 12); //to do la conexion con session 
-?>
