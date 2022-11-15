01a4d53e89432a7dd56283786e3125adc5e2aebd
SrDoger <74317397+SrDoger@users.noreply.github.com>
2022-11-15 14:32:11 -0300

    fix

--- ASIO/Buscador/admin/replay.php
+++ ASIO/Buscador/admin/replay.php
@@ -1,6 +1,8 @@
 <?php
 require("../classes/Session.php");
+require_once("../forms/forms.php");
 $session = new session();
+$formulario = new formulario();
 if ($_SESSION["admin"] == 1) {
 ?>
     <html>
@@ -14,42 +16,54 @@ if ($_SESSION["admin"] == 1) {
         <link rel="stylesheet" href="../assets/fonts/font-awesome.min.css">
         <link rel="stylesheet" href="../assets/fonts/simple-line-icons.min.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+        <style>
+            p {
+                color: greenyellow;
+            }
+
+            table,
+            th,
+            td {
+                background-color: black;
+                border: 1px solid blue;
+            }
+        </style>
     </head>
 
     <body>
         <nav class="navbar navbar-light navbar-expand bg-light navigation-clean">
 
             <?php
-            $session->setvalor("locate", "../");
             $session->isConnect();
             ?>
-
         </nav>
         <header class="text-center text-white masthead" style="background: url(&quot;../assets/img/nis/idea1.png&quot;) center / 200px repeat;">
             <div class="overlay"></div>
             <div class="container">
                 <div class="row">
                     <div class="col-xl-9 mx-auto position-relative">
-                        <h1 class="mb-5">Seccion de Administracion</h1>
+                        <h1 class="mb-5">Tabla de usuarios</h1>
+                        <form action="replay.php?" method="get">
+                            <input type="text" name="query" id="query" value = "AdminDeleteCount" hidden>
+                            <input type="number" name="id" id="id">
+                            <button type="submit">Enviar</button>
+                        </form>
+                        <?php
+                        //$formulario->typeOfForm($_GET["type"]); to do mostrar todas las consultas por aca y cambiar nombre al mismo archivo
+                        $formulario->typeOfForm($_GET["query"]);
+
+
+                        ?>
                     </div>
                     <div class="col-md-10 col-lg-8 col-xl-7 mx-auto position-relative">
-                        <ol>
-                            <li><a href="replay.php?query=showAllUsers">Ver lista de usuarios</a></li>
-                            <li><a href="replay.php?query=deleteCount">Buscar usuario por mail</a></li>
-                            <li><a href="replay.php?query=">Buscar usuario por id</a></li>
-                        </ol>
-                        <ol>
-                            <h2>Por ID</h2>
-                            <li><a href="replay.php?query=AdminDeleteCount">eliminar usuario</a></li>
-                            <li><a href="replay.php?query=">Cambiar contraseña de usuario</a></li>
 
-                        </ol>
-                        <li><a href="../index.php">volver al inicio</a></li>
+                        <li><a href="index.php">volver al atras</a></li>
                     </div>
                 </div>
             </div>
         </header>
         <footer class="bg-light footer">
+
         </footer>
         <script src="../assets/js/bootstrap.min.js"></script>
         <script src="../assets/js/script.min.js"></script>
