b89f4c347d5a86d462160862be44bd722ba70301
SrDoger <allanjmontero@gmail.com>
2022-11-22 13:58:04 -0300

    0.1.14
    Sistema de seguridad para evitar inyecciones, ademas de un arreglo a un bug por no estar ingresado

--- ASIO/Buscador/Buscadores.php
+++ ASIO/Buscador/Buscadores.php
@@ -2,9 +2,10 @@
 
 require_once("classes/Buscadores.php");
 require_once("classes/Conexion.php");
-
-$user = new usuario();
-$user->SaveSearch($_POST['element']);
+if (isset($_POST["id"])) {
+  $user = new usuario();
+  $user->SaveSearch($_POST['element']);
+}
 if (isset($_POST['ebay'])) {
   $ebay = new ebay();
   $ebay->Search($_POST['element']);
@@ -13,4 +14,3 @@ if (isset($_POST['merc'])) {
   $merc = new merc();
   $merc->Search($_POST['element']);
 }
-?>
--- ASIO/Buscador/classes/Conexion.php
+++ ASIO/Buscador/classes/Conexion.php
@@ -13,6 +13,10 @@ class BDD
 
   function ifExist($DBvalue, $table, $value)
   {
+    $value = $this->clearString($value);
+    $table = $this->clearString($table);
+    $DBvalue = $this->clearString($DBvalue);
+
     $condition = true;
     $query = "SELECT " . $DBvalue . " FROM " . $table . " WHERE " . $DBvalue . " = " . "'" . $value . "'";
     $result = $this->conn->query($query);
@@ -20,6 +24,10 @@ class BDD
       $condition = false; // no pueden existir 2 cuentas con mismo mail
 
     return $condition;
+  }
+  function clearString($value)
+  {
+    return mysqli_real_escape_string($this->conn, $value);
   }
 }
 
@@ -31,7 +39,7 @@ class usuario extends BDD
     if (isset($mail) && isset($pwd)) {
       $session = new session();
       //to do: guardar ip de las conexiones en una bdd $_SERVER['REMOTE_ADDR']; 
-
+      $pwd = $this->clearString($pwd);
       $query = "SELECT * FROM usuarios WHERE mail='" . $mail . "' and pwd='" . md5($pwd) . "'";
       $envio = $this->conn->query($query);
       print_r($envio->num_rows);
@@ -61,6 +69,9 @@ class usuario extends BDD
 
   function register($mail, $nombre, $pwd)
   {
+    $mail = $this->clearString($mail);
+    $nombre = $this->clearString($nombre);
+    $pwd = $this->clearString($pwd);
     $condition = $this->ifExist("mail", "usuarios", $mail);
     if (isset($mail) && isset($nombre) && isset($pwd) && $condition) {
       $query = "INSERT INTO usuarios(id,mail,nombre,pwd) VALUES (NULL,'" . $mail . "','" . $nombre . "','" . md5($pwd) . "')";
@@ -72,6 +83,7 @@ class usuario extends BDD
   }
   function delete($pwd)
   {
+    $pwd = $this->clearString($pwd);
     $session = new session();
     if (md5($pwd) == $_SESSION["pwd"]) {
       $query = "DELETE FROM usuarios WHERE id='" . $_SESSION["id"] . "'";
@@ -82,6 +94,8 @@ class usuario extends BDD
   }
   function userChangeMail($newmail, $pwd)
   {
+    $newmail = $this->clearString($newmail);
+    $pwd = $this->clearString($pwd);
     $session = new session();
 
     if ((md5($pwd)) == $_SESSION["pwd"] && $this->ifExist("mail", "usuarios", $newmail)) {
@@ -95,6 +109,8 @@ class usuario extends BDD
   {
     $session = new session();
     if ($_SESSION["pwd"] == md5($oldpwd)) {
+      $oldpwd = $this->clearString($oldpwd);
+      $newpwd = $this->clearString($newpwd);
 
       $query = "UPDATE usuarios SET pwd='" . md5($newpwd) . "' WHERE id='" . $_SESSION["id"] . "'";
       $this->conn->query($query);
@@ -106,6 +122,7 @@ class usuario extends BDD
 
   function SaveSearch($busqueda)
   {
+    $busqueda = $this->clearString($busqueda);
     $session = new session();
     $query = "INSERT INTO historialdebusquedas(id, consulta) VALUES (" . $_SESSION["id"] . ",'" . $busqueda . "')";
     $this->conn->query($query);
@@ -114,6 +131,7 @@ class usuario extends BDD
   //                         Funciones Administradoras
   function AdminUserDelete($id)
   {
+    $id = $this->clearString($id);
     if ($_SESSION["admin"] == 1) {
       $session = new session();
       $query = "DELETE FROM carrito WHERE id_usuario='" . $id . "'";
@@ -126,18 +144,22 @@ class usuario extends BDD
   }
   function AdminUserCar($id)
   {
+    $id = $this->clearString($id);
     if ($_SESSION["admin"] == 1) {
       header("location:../pdfTicket.php?id=" . $id);
     }
   }
   function AdminUserRecord($id)
   {
+    $id = $this->clearString($id);
     if ($_SESSION["admin"] == 1) {
       header("Location:../error.php?error=Todavia no existe la lectura de historial");
     }
   }
   function AdminUserChangePWD($id, $pwd)
   {
+    $id = $this->clearString($id);
+    $pwd = $this->clearString($pwd);
     if ($_SESSION["admin"] == 1) {
       $query = "UPDATE usuarios SET pwd='" . md5($pwd) . "' WHERE id='" . $id . "'";
       $this->conn->query($query);
@@ -146,6 +168,8 @@ class usuario extends BDD
   }
   function AdminUserChangeMail($id, $mail)
   {
+    $id = $this->clearString($id);
+    $mail = $this->clearString($mail);
     if ($_SESSION["admin"] == 1) {
       if ($this->ifExist("mail", "usuarios", $mail)) {
         $query = "UPDATE usuarios SET mail='" . $mail . "' WHERE id='" . $id . "'";
@@ -156,7 +180,10 @@ class usuario extends BDD
     }
   }
   function AdminUserChangeNickName($id, $nickName)
+
   {
+    $id = $this->clearString($id);
+    $nickName = $this->clearString($nickName);
     if ($_SESSION["admin"] == 1) {
       $query = "UPDATE usuarios SET nombre='" . $nickName . "' WHERE id='" . $id . "'";
       $this->conn->query($query);
@@ -165,6 +192,7 @@ class usuario extends BDD
   }
   function showSearchHistory($id)
   {
+    $id = $this->clearString($id);
     if ($_SESSION["admin"] == 1) {
 
       $query = "SELECT * from historialdebusquedas where id=" . $id;
